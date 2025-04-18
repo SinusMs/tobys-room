01ebd3e082b965622fe7282a6610fff16cc54e86
SinusMs <sinus.ms@gmx.de>
2025-04-18 18:24:06 +0200

    remove minigame + darkness shader

--- levels/main_level.gd
+++ levels/main_level.gd
@@ -1,29 +1,16 @@
 extends Node2D
 
 var rauchwolke_scene : PackedScene = preload("res://entities/rauchwolke_cpu_particles.tscn")
-var screen_tint_scene : PackedScene = preload("res://entities/kein_minigame/screen_tint.tscn")
-var trash_scene : PackedScene = preload("res://entities/kein_minigame/draggable_object.tscn")
-var usable_trash_can_scene : PackedScene = preload("res://entities/kein_minigame/usable_trashcan.tscn")
-var item_description : ItemBase = preload("res://entities/kein_minigame/description_item.tscn").instantiate()
-var items_bis_km : int = 15
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	SignalBus.trash_removed.connect(remove_trash)
-	SignalBus.item_picked_up.connect(spawn_trash)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
 
 func item_play_sound(string : String = "") -> void:
 	if Utils.currently_selected_item == null:
 		return
 	$AudioStreamPlayer.stream = Utils.currently_selected_item.sound
 	$AudioStreamPlayer.play()
-
 
 func _spawn_children(item : ItemBase) -> bool:
 	var children : Dictionary = item.children
@@ -35,51 +22,15 @@ func _spawn_children(item : ItemBase) -> bool:
 		%ItemContainer.add_child(child_scene)
 	return true
 
-
 func _spawn_particles(item : ItemBase) -> void:
 	var rauchwolke: CPUParticles2D = rauchwolke_scene.instantiate()
 	rauchwolke.position = item.position + item.mittelpunkt
 	rauchwolke.emitting = true
 	%ParticleContainer.add_child(rauchwolke)
 
-var trash_amount : int = 0
-func spawn_trash(item : ItemBase) -> void:
-	items_bis_km -= 1
-	if items_bis_km != 0:
-		return
-	trash_amount = 8
-	Utils.currently_selected_item = item_description
-	$Menu.change_item("")
-	var screen_tint : TextureRect = screen_tint_scene.instantiate()
-	%KeinMinigameContainer.add_child(screen_tint)
-	var usable_trash_can : Area2D = usable_trash_can_scene.instantiate()
-	%KeinMinigameContainer.add_child(usable_trash_can)
-	usable_trash_can.position = Vector2(60, 590)
-	for i : int in trash_amount:
-		var trash : CharacterBody2D = trash_scene.instantiate()
-		%KeinMinigameContainer.add_child(trash)
-		trash.position = Vector2(randi_range(64, 480), randi_range(64, 576))
-
-
-func remove_trash(trash : CharacterBody2D) -> void:
-	item_play_sound()
-	var rauchwolke: CPUParticles2D = rauchwolke_scene.instantiate()
-	rauchwolke.position = trash.position
-	rauchwolke.emitting = true
-	%ParticleContainer.add_child(rauchwolke)
-	trash.queue_free()
-	trash_amount -= 1
-	if trash_amount <= 0:
-		cleanup()
-
-
 func cleanup() -> void:
-	for child in %KeinMinigameContainer.get_children():
-		child.queue_free()
-	items_bis_km = 15
 	Utils.currently_selected_item = null
 	$Menu.change_item("")
-
 
 func end_this() -> void:
 	for i in 100:
